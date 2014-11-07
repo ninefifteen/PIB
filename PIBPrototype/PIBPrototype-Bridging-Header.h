84738e25356bef97672e030d1647bfe54ff46c66
Shawn Seals <shawn.seals915@yahoo.com>
2014-11-07 14:59:51 -0600

    libxml2 imports added to bridging header.

--- PIBPrototype/PIBPrototype-Bridging-Header.h
+++ PIBPrototype/PIBPrototype-Bridging-Header.h
@@ -8,6 +8,12 @@
 
 #import "CorePlot-CocoaTouch.h"
 
+#import <libxml/tree.h>
+#import <libxml/parser.h>
+#import <libxml/HTMLparser.h>
+#import <libxml/xpath.h>
+#import <libxml/xpathInternals.h>
+
 #ifndef PIBPrototype_PIBPrototype_Bridging_Header_h
 #define PIBPrototype_PIBPrototype_Bridging_Header_h
 
