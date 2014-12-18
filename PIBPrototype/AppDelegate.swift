6a967799627d2ba6168b66b4c1fed7998933968d
Shawn Seals <shawn.seals915@yahoo.com>
2014-12-17 19:38:30 -0600

    Launch image title changed.

--- PIBPrototype/AppDelegate.swift
+++ PIBPrototype/AppDelegate.swift
@@ -94,7 +94,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     // MARK: - Core Data stack
 
     lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.shawnseals915.PIBPrototype" in the application's documents Application Support directory.
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.scoutly.PIBPrototype" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1] as NSURL
     }()
