29b3e114a7debdcb6aacd860eae97f9fb949ae75
Shawn Seals <shawn.seals915@yahoo.com>
2015-01-13 23:25:54 -0600

    Analytics turned on.

--- PIB/AppDelegate.swift
+++ PIB/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-let logAnalytics: Bool = false
+let logAnalytics: Bool = true
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
