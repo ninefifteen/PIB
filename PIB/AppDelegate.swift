ce90c4bc9ed96fe3aadb2f84ec3a9d79dcc226b3
Shawn Seals <shawn.seals915@yahoo.com>
2015-07-10 08:40:51 -0500

    Persistent Store url changed to prevent crash on data migration.

--- PIB/AppDelegate.swift
+++ PIB/AppDelegate.swift
@@ -131,7 +131,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
         var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("PIB.sqlite")
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("PIB_v1_0.sqlite")
         var error: NSError? = nil
         var failureReason = "There was an error creating or loading the application's saved data."
         let options = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true]
